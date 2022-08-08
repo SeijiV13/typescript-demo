97ff3ee2684226a77003a566abf6b88b1cfc9cfd
Seiji Villafranca <villafrancasra@gmail.com>
2022-08-08 14:04:43 +0800

    add changes to typescript

--- index.ts
+++ index.ts
@@ -34,5 +34,115 @@ export enum Actions {
 interface Cat {
     name: string;
     breed: string;
-    eat: (food: string) => {}
+    eat?: (food: string) => string;
 }
+
+// var cat: Cat[] = [];
+var cat: string[] = [];
+export const addCat = (data: string) => {
+    cat.push(data);
+    displayCats();
+
+}
+
+export const displayCats = () => {
+    let list = document.getElementById("myList");
+    if(list) {
+        while(list.hasChildNodes() && list.firstChild) {
+            list.removeChild(list.firstChild);
+        }
+    }
+    cat.forEach((item: string) => {
+        let li = document.createElement("li");
+        li.innerText = item;
+        if(list) {
+            list.appendChild(li);
+        }
+    })
+}
+
+export const loopCats = () => {
+    // for loop
+   for(let i = 0; i < cat.length; i++) {
+    console.log(cat[i]);
+   }
+
+   // for (short hand)
+   for(let data of cat) {
+      console.log("using of for")
+      console.log(data);
+   }
+
+   // for (short hand but using index)
+   for(let data in cat ) {
+    console.log("using in for")
+    console.log(cat[data]);
+   }
+   // while
+   let i = 0
+   while(i < cat.length) {
+    console.log("using while")
+    console.log(cat[i]);
+    i++;
+   }
+
+
+//    cat.forEach((data: Cat) => {
+//     console.log("using forEach")
+//     console.log(data)
+//    });
+//    cat.map((data: Cat) => {
+//     console.log("using map")
+//     console.log(data);
+//    })
+}
+
+export const eatFood =(food: string) => {
+    return "I ate " + food + ", I am full!"
+}
+
+var dogs: string[] = [];
+export const addDogs = (...dogArray: string[]) => {
+    for(let data of dogArray) {
+        dogs.push(data);
+    }
+    console.log(dogs);
+}
+
+export const exampleIf = () => {
+    var cond = true;
+    if(cond) {
+
+    } 
+    else  {
+
+    }
+}
+
+export const exampleCase = (value: string) => {
+   switch(value) {
+    case "case 1" :
+    break;
+    case "case 2" :
+
+    break;
+    default:
+   }
+}
+
+var tuple: (string | number)[]  = ["", 1];
+
+var newCat: Cat = {
+    name: "Garfield", 
+    breed: "Siberian", 
+    eat: () => eatFood("Chicken")
+    }
+
+
+// addCat(newCat);
+// addDogs("Dog1", "Dog2", "Dog3");
+// newCat.name = "Tom"
+// addCat(newCat);
+
+// loopCats();
+
